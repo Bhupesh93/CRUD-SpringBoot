ffd327c3f22410287681b0efa2f487c3391ad380
Bhupesh S <bhupshs@amazon.com>
2018-01-06 19:11:22 +0530

    init repo

---
+++ src/main/java/com/crud/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.crud.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.crud.springboot"})
+public class Application {
+    public static void main(String[] arg){
+        SpringApplication.run(Application.class,arg);
+    }
+}
