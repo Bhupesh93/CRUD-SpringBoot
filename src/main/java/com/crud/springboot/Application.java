f4a729a4dab06327deeee0f3445c84bee3e197d6
Bhupesh S <bhupshs@amazon.com>
2018-01-06 19:16:06 +0530

    dummy commit 1

--- src/main/java/com/crud/springboot/Application.java
+++ src/main/java/com/crud/springboot/Application.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] arg){
         SpringApplication.run(Application.class,arg);
+        //dummy 1 changes
     }
 }
